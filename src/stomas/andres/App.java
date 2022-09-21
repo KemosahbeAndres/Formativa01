8c8e43f9da9b68d79f6e7241b52ff06d722aa32d
Andres Cubillos S <andrestj1996@gmail.com>
2022-09-20 20:01:30 -0400

    Verificacion rut terminada.

--- src/stomas/andres/App.java
+++ src/stomas/andres/App.java
@@ -1,6 +1,7 @@
 package stomas.andres;
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        Nacional cd = new Nacional();
+        System.out.println(cd.validRun("20919788-k"));
     }
 }
--- src/stomas/andres/Nacional.java
+++ src/stomas/andres/Nacional.java
@@ -1,10 +1,65 @@
 package stomas.andres;
 
+import java.util.Arrays;
+
 public class Nacional extends Cedula{
     public String getRut(){
         return super.getRun();
     }
     public void setRut(String run){
-        super.setRun(run.trim());
+        if(validRun(run)){
+            System.out.println("run valido "+ run);
+            super.setRun(run);
+        }
+    }
+    public boolean validRun(String run){
+        if(run.length() < 9){
+            System.out.println("El run tiene el formato incorrecto (12.345.678-0)");
+            return false;
+        }
+        if(run.contains(".")){
+            run.replace(".", "");
+        }
+        String rut = run.split("-")[0];
+        String dv = run.split("-")[1];
+        int[] numeros = {2, 3, 4, 5, 6, 7};
+        int[] validadores = new int[rut.length()];
+        int j = 0;
+        for(int i = 0; i < validadores.length; i++){
+            if(j==numeros.length) j = 0;
+            validadores[i] = numeros[j];
+            j++;
+            //System.out.print(validadores[i] + ", ");
+        }
+        //System.out.println("");
+        String reversed = "";
+        for(int i = rut.length()-1; i >= 0; i--){
+            reversed += rut.charAt(i);
+        }
+        //System.out.println(reversed);
+        int suma = 0;
+        for(int i = 0; i < rut.length(); i++){
+            suma += validadores[i] * Integer.parseInt(String.valueOf(reversed.charAt(i)));
+        }
+        //System.out.println(suma);
+        double A = suma / 11;
+        //System.out.println(A);
+        int B = ((int) A) * 11;
+        //System.out.println(B);
+        int C = Math.abs(suma - B);
+        //System.out.println(C);
+        int DV = 11 - C;
+        String fdv = "";
+        if(DV == 10){
+            fdv = "k";
+        }else if (DV == 11){
+            fdv = String.valueOf(0);
+        }else{
+            fdv = String.valueOf(DV);
+        }
+        //System.out.println("DV calculado: " + finalDV);
+        //System.out.println(dv);
+        if( fdv.contentEquals(dv.toLowerCase()) ) return true;
+        return false;
     }
 }
